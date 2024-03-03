f6f0f7233e1c7dfc39abce5e32bb5df57bfef2ca
Parker <parker_outerspace@hotmail.com>
2024-03-03 17:34:02 -0500

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LandingPage from './components/LandingPage';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Landing' ///the name of the initial screen
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Landing" component={LandingPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
