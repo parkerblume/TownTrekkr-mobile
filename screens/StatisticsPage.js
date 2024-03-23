6608a89762ea3c962bd47ae2c3cd511e1dfc761a
David Figueras <david.f.sosa33@gmail.com>
2024-03-23 16:26:15 -0400

    Add: Back button and started top box

--- screens/StatisticsPage.js
+++ screens/StatisticsPage.js
@@ -11,9 +11,22 @@ const StatisticsPage = ( {navigation} ) => {
   return (
     <View style={styles.container}>
 
-      <StatusBar backgroundColor='#abc4ab' />
+      <StatusBar backgroundColor={colors.tan} />
+
+
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate("Login")}>
+        <Text style={styles.backButtonText}>&lt;</Text>
+      </TouchableOpacity>
 
       
+      <View style={styles.townStatBox}>
+        {/* TODO: ADD DROP DOWN SELECTOR */}
+        <View style={styles.townStatTitleContainer}>
+          <Text>TestTown</Text>
+          <Text> Statistics</Text>
+        </View>
+
+      </View>
 
     </View>
   );
@@ -28,6 +41,38 @@ const styles = StyleSheet.create({
       flexWrap: 'nowrap',
       justifyContent: 'flex-start',
       alignItems: 'center',
-      backgroundColor: '#abc4ab',
+      backgroundColor: colors.tan,
+    },
+    backButtonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+      alignContent: 'flex-start'
+    },
+    backButton: {
+      marginTop: 50,
+      alignSelf: 'flex-start',
+      marginLeft: 32,
+    },
+    backButtonText:
+    {
+      color: colors.buttonPrimary,
+      fontSize: 40,
+      fontWeight: 'bold',
+    },
+    townStatBox: {
+      width: 300,
+      height: 200,
+      backgroundColor: colors.background,
+      borderRadius: 20,
+      marginTop: 10,
+      marginLeft: 110,
+      borderWidth: 1,
+    },
+    townStatTitleContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignContent: 'center',
     },
   });
