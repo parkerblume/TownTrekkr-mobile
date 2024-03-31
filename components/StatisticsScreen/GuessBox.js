349f838efe87a0af0441df50beab8d68e6ab3850
David Figueras <david.f.sosa33@gmail.com>
2024-03-31 19:30:08 -0400

    Create GuessBox.js

---
+++ components/StatisticsScreen/GuessBox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity,
+         KeyboardAvoidingView, Keyboard } from 'react-native';
+import { colors } from '../styles/commonStyles';
+
+
