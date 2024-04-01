d96374c11d68f9653d14000e1c7dbf232f046f66
David Figueras <david.f.sosa33@gmail.com>
2024-04-01 13:47:52 -0400

    Created Recent Guess Box component

--- components/StatisticsScreen/GuessBox.js
+++ components/StatisticsScreen/GuessBox.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity,
-         KeyboardAvoidingView, Keyboard } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity } from 'react-native';
 import { colors } from '../styles/commonStyles';
 
 
+const GuessBox = () => {
+
+
+    return (
+        <>
+            <View style={{width: 75, height: 75, backgroundColor: 'white', opacity: 0.5, borderRadius: 10, marginRight: 10, borderWidth: 1}}></View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default GuessBox;
