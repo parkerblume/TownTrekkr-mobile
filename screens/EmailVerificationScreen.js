2bf6ad6298206c6cde3e89f24d6df6fb2631f3e5
David Figueras <david.f.sosa33@gmail.com>
2024-04-02 18:42:49 -0400

    Add: Basic verification code functionality

--- screens/EmailVerificationScreen.js
+++ screens/EmailVerificationScreen.js
@@ -9,6 +9,43 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const EmailVerificationScreen = ( {navigation} ) => {
 
+  const [inputs, setInputs] = React.useState(Array(4).fill(''));
+
+  const sendCodeToEmail = () => {
+    console.log("Code sent to email");
+  };
+
+  React.useEffect(() => {
+    sendCodeToEmail();
+  });
+
+  const handleInputChange = (text, index) => {
+
+    // Update input array
+    const newInputs = [...inputs];
+    newInputs[index] = text;
+
+    // Move focus to the next input box
+    if (index < 3 && text.length === 1)
+    {
+      inputRefs[index + 1].current.focus();
+    }
+    else if (text === '' && index > 0)
+    {
+      inputRefs[index - 1].current.focus();
+    }
+
+    setInputs(newInputs);
+    
+  };
+
+  const getCode = () => {
+    return inputs.join('');
+  };
+
+  // Array to hold refs for each TextInput
+  const inputRefs = Array(4).fill().map((_, i) => React.useRef(null));
+
 
   return (
     <KeyboardAvoidingView style={styles.container}
@@ -27,6 +64,22 @@ const EmailVerificationScreen = ( {navigation} ) => {
 
 
 
+      {/* input field for verification code */}
+      <View style={{flexDirection: 'row', marginTop: '20%'}}>
+      {inputs.map((value, index) => (
+        <TextInput
+          key={index}
+          ref={inputRefs[index]}
+          style={styles.inputBox}
+          maxLength={1}
+          keyboardType="numeric"
+          value={value}
+          onChangeText={(text) => handleInputChange(text, index)}
+        />
+      ))}
+      </View>
+
+      <Text>Code inputed: {getCode()}</Text>
 
       
       {/* confirm button */}
@@ -36,14 +89,28 @@ const EmailVerificationScreen = ( {navigation} ) => {
 
 
       {/* Text under confirm button */}
-      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-            <Text>
+      <View style={{alignItems: 'center'}}>
+            <Text style={styles.dontSeeCodeText}>
                 Don't see the code?
             </Text>
-            <Text style={{fontWeight: 'bold', textDecorationLine: 'underline'}}>
-              Register here
+            <Text style={styles.sendAgainText}>
+
+              <TouchableOpacity onPress={() => sendCodeToEmail()}>
+                <Text style={{fontWeight: 'bold', textDecorationLine: 'underline'}}>
+                  Send again
+                </Text>
+              </TouchableOpacity>
+
+              <Text>&nbsp;or&nbsp;</Text>
+
+              <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+                <Text style={{fontWeight: 'bold', textDecorationLine: 'underline'}}>
+                  register with new email
+                </Text>
+              </TouchableOpacity>
+
             </Text>
-      </TouchableOpacity>
+      </View>
 
     </KeyboardAvoidingView>
   );
@@ -69,6 +136,17 @@ const styles = StyleSheet.create({
       fontFamily: 'Londrina-Solid-Light',
       fontSize: 20,
     },
+    inputBox: {
+      width: 50,
+      height: 50,
+      borderWidth: 1,
+      borderColor: 'gray',
+      textAlign: 'center',
+      fontSize: 20,
+      fontFamily: 'Londrina-Solid-Light',
+      marginHorizontal: 5,
+      borderRadius: 10,
+    },
     confirmButton: {
       marginTop: '20%',
       alignItems: 'center',
@@ -82,5 +160,16 @@ const styles = StyleSheet.create({
       color: 'white',
       fontSize: 30,
       fontFamily: 'Londrina-Solid',
+    },
+    dontSeeCodeText: {
+      fontFamily: 'Londrina-Solid-Light',
+      fontSize: 20,
+      color: 'black',
+      marginTop: 20,
+    },
+    sendAgainText: {
+      fontFamily: 'Londrina-Solid-Light',
+      fontSize: 20,
+      color: 'black',
     },
   });
